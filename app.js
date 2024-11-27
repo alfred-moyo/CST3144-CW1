253481a2009e96faaa7bcbd6075f4a47eb260453
alfred-moyo <alf.moxz@gmail.com>
2024-11-27 23:43:59 +0400

    updated app.js, index.html and style.css

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ let app = new Vue({
   el: '#app',
   data: {
     appURL: 'http://localhost:3000',
-    //  'https://mathisihub.onrender.com',
+    // appURL: 'https://mathisihub.onrender.com',
     cart: [], 
     classList: [],
     showPage: true,
     selectedSubject: '',
     sortOption: 'asc',
+    searchQuery: '',
     uniqueSubjects: ['Maths', 'Science', 'English'],
     order: {
       name: "",
@@ -22,6 +23,7 @@ let app = new Vue({
         const cartCount = this.cart.filter(item => item.id === classList.id).length;
         return classList.spaces > cartCount;
       },
+      
       addToCart(classList) {
         if (this.canAddtoCart(classList)) {
           this.cart.push(classList);
@@ -42,12 +44,47 @@ let app = new Vue({
       },
       submitCheckOut() {
         alert("Order placed successfully!")
-      }
+      },
+      refreshLessons() {
+        fetch(`${this.appUrl}/lessons`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`Failed to fetch programs. Status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then(data => {
+                this.classList = data;
+                console.log("Lessons updated:", this.classList);
+            })
+            .catch(err => {
+                console.error('Error fetching programs:', err);
+            });
+      },
+      searchLessons() {
+        if (!this.searchQuery.trim()) {
+            this.refreshLessons();
+            return;
+        }
+
+        fetch(`${this.appUrl}/search?term=${encodeURIComponent(this.searchQuery)}`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`Failed to fetch search results. Status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then(data => {
+                this.classList = data; // Update programs with search results
+                console.log("Search results:", data);
+            })
+            .catch(err => {
+                console.error('Error searching programs:', err);
+            });
+    },
   },
     computed: {
-      // uniqueSubjects() {
-      //   return [...new Set(this.classList.map(item => item.subject))];
-      // },
+      
       filteredAndSortedClasses() {
         let filtered = this.classList;
         if (this.selectedSubject) {
